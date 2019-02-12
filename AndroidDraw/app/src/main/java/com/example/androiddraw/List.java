5e2965892f49c6eefec31f53889fcadaf48af9e6
Shupegin <ALT7024@yandex.ru>
2019-02-12 21:09:02 +0500

    Add list

--- AndroidDraw/app/src/main/java/com/example/androiddraw/List.java
+++ AndroidDraw/app/src/main/java/com/example/androiddraw/List.java
@@ -1,20 +1,33 @@
 package com.example.androiddraw;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 public class List extends ListActivity {
+    final String[] catNamesArray = new String[] { "Рыжик", "Барсик", "Мурзик",
+            "Мурка", "Васька", "Томасина", "Бобик", "Кристина", "Пушок",
+            "Дымка", "Кузя", "Китти", "Барбос", "Масяня", "Симба" };
 
-    String[] names = {"Андрей", "Ваня", "Сергей", "Олег"};
+
     private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, names);
+        mAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, catNamesArray);
         setListAdapter(mAdapter);
     }
 
 }
+
+
+
+
+
+
